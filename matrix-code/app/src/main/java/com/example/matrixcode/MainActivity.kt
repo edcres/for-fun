63099273eca56c3babd015e109e28aeb4c93c52f
Ed Cres <crespoed07@gmail.com>
2022-03-30 23:08:02 -0400

    Set up and planned put the matrix app

--- matrix-code/app/src/main/java/com/example/matrixcode/MainActivity.kt
+++ matrix-code/app/src/main/java/com/example/matrixcode/MainActivity.kt
@@ -7,16 +7,24 @@ import java.util.*
 
 // A cool way: https://www.youtube.com/watch?v=25AUSTtob6g
 
-// 39 x 48
+/**
+ *
+ *
+ * future:
+ *  - display a consecutive vertical gap between characters
+ *      - maybe manually input " " withing the same index 1 to 4 consecutive times
+ */
 
 class MainActivity : AppCompatActivity() {
 
+    private val xAxisChars = 39
+    private val yAxisChars = 48
     private lateinit var matrixTxt: TextView
     private val allLetters = listOf(
         "Z", "X", "->", "$", "*", "0", "1", "0", "1", "0", "1", "0", "1", "8", "&", "@", "]{", "^", "~", "<", ">", "+",
         " ", " "," "," "," "," "
     )
-    private val yAxisQueue: LinkedList<List<String>> = LinkedList<List<String>>()
+    private val yAxisQueue: LinkedList<String> = LinkedList<String>()
     // add/remove
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,21 +33,29 @@ class MainActivity : AppCompatActivity() {
 
         matrixTxt = findViewById(R.id.matrix_txt)
 
+        // do an infinite while loop, populateY(), populateMatrixTxt, short delay, repeat
+    }
 
-
-
+    private fun populateMatrixTxt() {
         var matrixString = ""
         for (i in 0 until yAxisQueue.size) {
-            matrixString = "${yAxisQueue[i]}$matrixString"
+            matrixString = "${yAxisQueue[i]}\n$matrixString"
         }
         matrixTxt.text = matrixString
     }
 
-    private fun populateX() {
-
+    private fun makeXAxis(): String {
+        var xAxisString = ""
+        for (i in 0 until xAxisChars) {
+            xAxisString = "$xAxisString ${/* todo: insert random letter from the 'allLetters' list */}"
+        }
+        return xAxisString
     }
 
-    private fun populateY(xAxisList: List<String>) {
-        yAxisQueue.add(xAxisList)
+    private fun populateY() {
+        yAxisQueue.add(makeXAxis())
+        if (yAxisQueue.size > yAxisChars) {
+            yAxisQueue.remove()
+        }
     }
 }
