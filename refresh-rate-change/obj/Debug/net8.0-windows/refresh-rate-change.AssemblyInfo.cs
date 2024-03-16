ed5ba956050ca9c26392df97d3b09d6354663eb7
Eduardo A. Crespo <crespoed07@gmail.com>
2024-03-16 19:32:16 -0400

    final edits.

--- refresh-rate-change/obj/Debug/net8.0-windows/refresh-rate-change.AssemblyInfo.cs
+++ refresh-rate-change/obj/Debug/net8.0-windows/refresh-rate-change.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("refresh-rate-change")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574a10d3e9cecc78c342cb0870dd86bd09dae92a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a653fd2fabe877a30df6ea3ee869cb95393d3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("refresh-rate-change")]
 [assembly: System.Reflection.AssemblyTitleAttribute("refresh-rate-change")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
