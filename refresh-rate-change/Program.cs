104b75789d7714ea28770e1123272bf2a7ed1639
Eduardo A. Crespo <crespoed07@gmail.com>
2024-03-15 23:23:45 -0400

    Detecting a change in display settings.

--- refresh-rate-change/Program.cs
+++ refresh-rate-change/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Win32;
+using System;
+
+namespace DisplaySettingsListener {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Listening for display settings changes...");
+            SystemEvents.DisplaySettingsChanged += OnDisplaySettingsChanged;
+            
+            // Prevents the application from exiting immediately
+            Console.WriteLine("Press 'Enter' to exit.");
+            Console.ReadLine();
+        }
+
+        private static void OnDisplaySettingsChanged(object sender, EventArgs e) {
+            // This is where you could add code to check and adjust the refresh rate if needed
+            Console.WriteLine("Display settings changed.");
+        }
+    }
+}
