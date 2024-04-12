4111a8076c09558018fc22be2ee13dae801a6a85
Eduardo A. Crespo <crespoed07@gmail.com>
2024-04-12 19:02:24 -0400

    Seiting up bird and gravity for avian flight game.

--- avian-flight/app/src/main/java/com/example/avianflight/MainActivity.kt
+++ avian-flight/app/src/main/java/com/example/avianflight/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.avianflight
 
+import GameView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val gameView = GameView(this)
+        setContentView(gameView)
     }
 }
+// TODO: handle this '?'
