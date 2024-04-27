815708e72bf47ab8d5fd327d66a8ed8c7c8a86f3
Eduardo A. Crespo <crespoed07@gmail.com>
2024-04-27 00:51:53 -0400

    Handled case in which appbar is null.

--- avian-flight/app/src/main/java/com/example/avianflight/MainActivity.kt
+++ avian-flight/app/src/main/java/com/example/avianflight/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.avianflight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val gameView = GameView(this)
+        supportActionBar?.hide() ?: Log.w("MainAct", "onCreate: Action bar is null")
         setContentView(gameView)
-        supportActionBar?.hide()
     }
 }
